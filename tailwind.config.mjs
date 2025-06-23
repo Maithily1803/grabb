ab0945bc8a9c03f1db69598cd334b5a7e2f6d607
maithilysawant <maithily1803@gmail.com>
2025-06-23 21:43:08 +0530

    update1

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import plugin from 'tailwindcss/plugin'
+
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [plugin()],
+}
+
+
